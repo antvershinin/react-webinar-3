2c9641ccb62ebdcee7c00ccf87a29922330c459b
Anton Vershinin <a.vershinin92@gmail.com>
2024-09-25 18:55:50 +0300

    added lang buttons

--- src/components/head/index.js
+++ src/components/head/index.js
@@ -1,11 +1,27 @@
 import { memo } from 'react';
 import PropTypes from 'prop-types';
 import './style.css';
+import { cn as bem } from '@bem-react/classname';
+import { useLocale } from '../../hooks/localization/locale-provider';
 
 function Head({ title }) {
+  const { langList, changeLang } = useLocale()
+  const cn = bem('Head');
+
+  const handleChangeLang = (lang) => {
+    changeLang(lang.code)
+  }
+
+  const renderLangButtons = () => {
+    return langList.map((lang,index) => (
+      <button onClick={e=>handleChangeLang(langList[index])}>{lang.name}</button>
+    ))
+  }
+
   return (
-    <div className="Head">
+    <div className={cn()}>
       <h1>{title}</h1>
+      <div className={cn('buttons-wrapper')}>{renderLangButtons()}</div>
     </div>
   );
 }
--- src/hooks/localization/locale-provider.js
+++ src/hooks/localization/locale-provider.js
@@ -4,15 +4,17 @@ import * as translations from './translations/translations.json'
 const LocaleContext = createContext()
 
 function LocaleProvider({children}) {
-    const [lang, setLang] = useState('eng')
+    const [lang, setLang] = useState('ru')
 
     const changeLang = (newLang) => {
         setLang(newLang)
     }
 
+    const langList = [{"code" : 'ru', "name" : "Русский"}, {"code" : 'eng', "name" : "English"}]
+
     const translate = (key) => translations[lang][key] 
 
-    const value = {lang, changeLang, translate}
+    const value = {lang, langList, changeLang, translate}
 
     return <LocaleContext.Provider value={value}>{children}</LocaleContext.Provider>
 }
